7d3815ff1c34b68bf88d41f5061f5bcefd7f2119
eveah-codes <52373568+eveah-codes@users.noreply.github.com>
2023-12-20 19:27:22 +0000

    Implemented create blog form

---
+++ src/BlogForm.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+export default function BlogForm(props) {
+     const [selectedFile, setSelectedFile] = React.useState(null);
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        setSelectedFile(file);
+    };
+  return (
+    <form className="blogForm" onSubmit={props.addBlog}>
+      <label htmlFor="postTitle">
+        Post Title<span className="required">*</span>
+      </label>
+      <input
+        type="text"
+        id="postTitle"
+        name="postTitle"
+        onChange={props.createBlog}
+        value={props.newBlog.postTitle}
+        required
+      />
+      <label htmlFor="postCategory">
+        Post Category<span className="required">*</span>
+      </label>
+      <select
+        name="postCategory"
+        id="postCategory"
+        onChange={props.createBlog}
+        value={props.newBlog.postCategory || ""}
+        required
+      >
+        <option value="" disabled hidden className="default-option">
+          Choose
+        </option>
+        <option value="Books">Books</option>
+        <option value="Fiction">Fiction</option>
+      </select>
+      <label htmlFor="featuredImage">
+        Featured Image<span className="required">*</span>
+        <p className="featured-img-text">Browse Photo</p>
+      </label>
+      <input
+        type="file"
+        id="featuredImage"
+        name="featuredImage"
+        accept="image/*"
+        onChange={props.createBlog}
+      />
+      <label htmlFor="postDescription">
+        Post Description<span className="required">*</span>
+      </label>
+      <textarea
+        name="postDescription"
+        id="postDescription"
+        cols="30"
+        rows="10"
+        value={props.newBlog.postDescription}
+        onChange={props.createBlog}
+        required
+      ></textarea>
+
+      <button className="formBtn" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+}
